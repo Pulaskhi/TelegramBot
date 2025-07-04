ca028a0cd0acf804d8894f76291b7df2580f0c05
Pulaskhi <mgmdestral@gmail.com>
2025-07-04 09:34:59 +0200

    crear promoters en el panel de admin

---
+++ client/admin/front-admin/src/components/forms/promoters-form-component.js
@@ -0,0 +1,405 @@
+import isEqual from 'lodash-es/isEqual'
+import { store } from '../../redux/store.js'
+import { refreshTable } from '../../redux/crud-slice.js'
+
+class PromoterForm extends HTMLElement {
+    constructor () {
+      super()
+      this.shadow = this.attachShadow({ mode: 'open' })
+      this.endpoint = '/api/admin/promoters'
+      this.unsubscribe = null
+      this.formElementData = null
+    }
+  
+    connectedCallback () {
+        this.unsubscribe = store.subscribe(() => {
+          const currentState = store.getState()
+    
+          if (currentState.crud.formElement.data && currentState.crud.formElement.endPoint === this.endpoint && !isEqual(this.formElementData, currentState.crud.formElement.data)) {
+            this.formElementData = currentState.crud.formElement.data
+            this.showElement(this.formElementData)
+          }
+          if (!currentState.crud.formElement.data && currentState.crud.formElement.endPoint === this.endpoint) {
+            this.resetForm()
+          }
+        })
+    
+        this.render()
+      }
+  
+    render () {
+      this.shadow.innerHTML =
+      /* html */`
+      <style>
+  
+        *{
+          box-sizing: border-box;
+        }
+  
+        h1, h2, h3, h4, h5, h6, p{
+          margin: 0;
+        }
+  
+        h1, h2, h3, h4, h5, h6, p, a, span, li, label, input, button{
+          font-family: "Nunito Sans", serif;
+          font-optical-sizing: auto;
+        }
+  
+        button{
+          background-color: transparent;
+          border: none;
+          cursor: pointer;
+          outline: none;
+          padding: 0;
+        }
+  
+        h1, h2, h3, h4, h5, h6, p{
+          margin: 0;
+        }
+  
+        h1, h2, h3, h4, h5, h6, p, a, span, li, label, input, button{
+          font-family: "Nunito Sans", serif;
+          font-optical-sizing: auto;
+        }
+  
+        img{
+          object-fit: cover;
+          width: 100%;
+        }
+  
+        ul{
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+        }
+  
+        .form__header-box{
+          display: flex;
+          justify-content: space-between; /* Alinea los elementos a los extremos */
+          align-items: center;
+          background: hsl(198, 100%, 85%);
+          border-radius: 5px;
+  
+        }
+  
+        .form__header-box-filter.active{
+          background: hsl(198, 74.20%, 6.10%);
+          padding: 5px 10px;
+          color: white; /* Asegura que el texto sea visible */
+          height: 30px;
+          border-radius: 5px 0 0 5px;
+  
+        }
+        .form__header-box-filter{
+          background: hsl(200, 77%, 42%);
+          padding: 5px 10px;
+          color: white; /* Asegura que el texto sea visible */
+          height: 30px;
+          border-radius: 5px 0 0 5px;
+  
+        }
+  
+        .form__header-box-filter button {
+          color: white; /* Asegura que el texto sea visible */
+          font-size: 16px;
+        }
+  
+        .form__header-icons {
+          display: flex;
+          gap: 10px; /* Espacio entre los iconos */
+          margin-right: 5px;
+        }
+  
+        .table__header__icon svg,
+        .edit-icon svg,
+        .delete-icon svg,
+        .clean-icon,
+        .save-icon {
+          width: 30px;
+          height: 30px;
+          fill: black;
+        }
+  
+  
+        /* Ajustar cada elemento del formulario */
+        .form-element {
+          flex: 1;
+          display:flex;
+          flex-direction: column;
+          gap: 10px 0px;
+          margin: 10px 0;
+        }
+    
+        .form-element-input input {
+          width: 100%;
+          padding: 10px;
+          border-radius: 5px;
+          box-sizing: border-box;
+          border: none;
+          background: white;
+          color: black;
+        }
+  
+        .tabs{
+          display:flex;
+          align-items: center;
+        }
+
+        .tab{
+          background: hsl(200, 77%, 42%);
+          cursor: pointer;
+          padding: 5px 10px;
+          color: white; 
+          height: 30px;
+        }
+
+        .tab:first-child{
+          border-radius: 5px 0 0 5px;
+        }
+
+        .tab button{
+          color: white; 
+        }
+
+        .tab.active{
+          background: hsl(198, 74.20%, 6.10%);
+          color: white; 
+        }
+
+        .tab-content{
+          display: none;
+        }
+
+        .tab-content.active{
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(20%, 1fr));
+          gap:1rem;
+        }
+        
+        .validation-errors.active{
+          background: hsl(0, 96.80%, 75.50%);
+          border-radius: 5px;
+          display: block;
+          margin: 1rem 0;
+          padding: 1rem;
+          position: relative;
+        }
+
+        .validation-errors{
+          display: none;
+        }
+
+        .close-validation-errors{
+          position: absolute;
+          right: 0.5rem;
+          top: 0.5rem;
+        }
+
+        .close-validation-errors svg{
+          fill: hsl(100, 100%, 100%);
+          height: 2rem;
+          width: 2rem;
+        }
+     
+      </style>
+  
+      <section class="form">
+        <div class="form__header">
+          <div class="form__header-box">
+          <div class="tabs">
+              <div class="tab active" data-tab="general">
+                <button>General</button>
+              </div>
+              <div class="tab" data-tab="images">
+                <button>Imágenes<button>
+              </div>
+            </div class="tabs">
+            <div class="form__header-icons">
+              <button class="clean-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <title>eraser</title>
+                  <path
+                    d="M16.24,3.56L21.19,8.5C21.97,9.29 21.97,10.55 21.19,11.34L12,20.53C10.44,22.09 7.91,22.09 6.34,20.53L2.81,17C2.03,16.21 2.03,14.95 2.81,14.16L13.41,3.56C14.2,2.78 15.46,2.78 16.24,3.56M4.22,15.58L7.76,19.11C8.54,19.9 9.8,19.9 10.59,19.11L14.12,15.58L9.17,10.63L4.22,15.58Z" />
+                </svg>
+              </button>
+              <button class="save-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <title>content-save</title>
+                  <path
+                    d="M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="form__body">
+          <div class="validation-errors">
+            <ul></ul>
+            <div class="close-validation-errors">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close-circle-outline</title><path d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" /></svg>
+            </div>
+          </div>
+          <form>
+            <input type="hidden" name="id">
+            <div class="tab-content active" data-tab="general">
+              <div class="form-element">
+                <div class="form-title">
+                  <span>Nombre</span>
+                </div>
+                <div class="form-element-input">
+                  <input type="text" placeholder="" name="name">
+                </div>
+              </div>
+              <div class="form-element">
+                <div class="form-title">
+                  <span>Email</span>
+                </div>
+                <div class="form-element-input">
+                  <input type="email" placeholder="" name="email">
+                </div>
+              </div>
+            </div>
+            <div class="tab-content" data-tab="images">
+              <div class="form-element">
+                <div class="form-title">
+                  <span>Avatar</span>
+                </div>
+                <div class="form-element-input">
+                  <input type="image" name="avatar">
+                </div>
+              </div>
+            </div>
+           
+          </form>
+        </div>
+      </section>
+      
+      `
+      // Se llama a la función para el botón de guardar
+      this.renderButtons()
+    }
+  
+    renderButtons () {
+      const SaveButton = this.shadow.querySelector('.save-icon')
+  
+      // async porque se hace un await para una llamada fetch
+      this.shadow.querySelector('.form').addEventListener('click', async event => {
+        // Prevenir que no se pasen los datos de los campo a través de la url.
+        event.preventDefault()
+
+        if(event.target.closest('.save-icon')){
+          const form = this.shadow.querySelector('form')
+          // Coge todos los valores de los inputs del formulario y te los prepara.
+          const formData = new FormData(form)
+          const formDataJson = {}
+    
+          for (const [key, value] of formData.entries()) {
+            formDataJson[key] = value !== '' ? value : null
+          }
+          const id = this.shadow.querySelector('[name="id"]').value
+          const endpoint = id ? `${this.endpoint}/${id}` : this.endpoint
+          const method = id ? 'PUT' : 'POST'
+          delete formDataJson.id
+          try {
+           
+            const response = await fetch(endpoint, {
+              method,
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(formDataJson)
+            })
+    
+            if (!response.ok) {
+              throw response
+            }
+  
+            store.dispatch(refreshTable(this.endpoint))
+            this.resetForm()
+            // const data = await response.json()
+    
+            document.dispatchEvent(new CustomEvent('notice', {
+              detail: {
+                message: 'Datos guardados correctamente',
+                type: 'success'
+              }
+    
+            }))
+          } catch (error) {
+            if (error.status === 422) {
+              const data = await error.json()
+              this.showValidationErrors(data.message)
+            }
+  
+            if (error.status === 500) {
+              document.dispatchEvent(new CustomEvent('notice', {
+                detail: {
+                  message: 'No se han podido guardar los datos',
+                  type: 'error'
+                }
+              }))
+            }
+          }
+        }
+  
+        if(event.target.closest('.clean-icon')){
+          this.resetForm()
+        }
+
+        if(event.target.closest('.tab')){
+          const clickedTab = event.target.closest('.tab')
+
+          this.shadow.querySelector('.tab.active').classList.remove('active')
+          clickedTab.classList.add('active')
+
+          this.shadow.querySelector('.tab-content.active').classList.remove('active')
+          this.shadow.querySelector(`.tab-content[data-tab='${clickedTab.dataset.tab}']`).classList.add('active')
+        }
+
+        if(event.target.closest('.close-validation-errors')){
+          this.hideValidationErrors()
+        }
+      })
+    }
+
+
+		showElement (data) {
+			Object.entries(data).forEach(([key, value]) => {
+				if (this.shadow.querySelector(`[name="${key}"]`)) {
+					this.shadow.querySelector(`[name="${key}"]`).value = value
+				}
+			})
+		}
+
+    showValidationErrors (messages) {
+
+      const errorlist = this.shadow.querySelector('.validation-errors ul')
+      errorlist.innerHTML = '' // Limpiar errores anteriores si hay
+
+
+      messages.forEach(error => {
+        const li = document.createElement('li'); 
+        li.textContent = error.message;          
+        errorlist.appendChild(li);                      
+      });
+      
+      const container = this.shadow.querySelector('.validation-errors')
+      container.classList.add('active')
+    }
+
+    hideValidationErrors(){
+      this.shadow.querySelector('.validation-errors').remove('active')
+    }
+	
+		resetForm () {
+			const form = this.shadow.querySelector('form')
+			form.reset()
+			this.shadow.querySelector('[name="id"]').value = ''
+			this.formElementData = null
+      this.hideValidationErrors()
+		}
+
+  }
+  
+  customElements.define('promoters-form-component', PromoterForm)
+  
---
+++ client/admin/front-admin/src/components/tables/promoters-table-component.js
@@ -0,0 +1,307 @@
+import { store } from '../../redux/store.js'
+import { showFormElement } from '../../redux/crud-slice.js'
+
+class PromoterTable extends HTMLElement {
+  constructor () {
+    super()
+    this.shadow = this.attachShadow({ mode: 'open' })
+    this.endpoint = '/api/admin/promoters'
+    this.filterQuery = null
+    this.unsubscribe = null
+  }
+
+  async connectedCallback () {
+    this.unsubscribe = store.subscribe(() => {
+      const currentState = store.getState()
+
+      if (currentState.crud.filterQuery.query && currentState.crud.filterQuery.endPoint === this.endpoint) {
+        this.filterQuery = currentState.crud.filterQuery.query
+        const endpoint = `${this.endpoint}?${currentState.crud.filterQuery.query}`
+        this.loadData(endpoint).then(() => this.render())
+      }
+
+      if (!currentState.crud.filterQuery.query && currentState.crud.tableEndpoint === this.endpoint) {
+        this.loadData().then(() => this.render())
+      }
+    })
+
+    await this.loadData()
+    await this.render()
+  }
+
+  async loadData (endpoint = this.endpoint) {
+    try {
+      const response = await fetch(endpoint)
+      if (!response.ok) {
+        throw new Error(`Error fetching data: ${response.statusText}`)
+      }
+      this.data = await response.json()
+    } catch (error) {
+      console.error('Error loading data:', error)
+      this.data = []
+    }
+  }
+
+  render () {
+    this.shadow.innerHTML = /*html*/`
+      <style>
+      * {
+        box-sizing: border-box;
+        font-family: "Nunito Sans", serif;
+      }
+
+      h1, h2, h3, h4, h5, h6, p {
+        margin: 0;
+      }
+
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+
+      button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+      }
+
+      .table {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        border-radius: 10px;
+      }
+
+      .table__header {
+        display: flex;
+        justify-content: flex-start;
+        background-color: hsl(198, 100%, 85%);
+        height: 30px;
+        border-radius: 5px;
+      }
+
+      .filter-icon,
+      .edit-icon,
+      .delete-icon,
+      .table-page-logo {
+        width: 30px;
+        height: 30px;
+        fill: black;
+      }
+
+      .table__body {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        width: 90%;
+        margin: 1rem auto;
+        max-height: 70vh;
+        overflow-y: auto;
+        padding-right: 1rem;
+      }
+
+      .user-box__data {
+        padding: 15px;
+        background-color: hsl(200, 77%, 32%);
+        border-radius: 0 0 10px 10px;
+        color: white;
+      }
+
+      .user-box__data li {
+        margin-top: 5px;
+      }
+
+      .user-box__upper-row {
+        display: flex;
+        justify-content: flex-end;
+        gap: 10px;
+        background-color: hsl(198, 100%, 85%);
+        border-radius: 10px 10px 0 0;
+      }
+
+      .table__footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 5px 10px;
+        background-color: hsl(198, 100%, 85%);
+        border-radius: 5px;
+        font-size: 0.95rem;
+      }
+
+      .table__footer span{
+        color: hsl(0, 0%, 0%);
+      }
+
+      .table__footer-left {
+        font-weight: 500;
+      }
+
+      .table__footer-right .pagination {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+      }
+
+      .pagination{
+        align-items: center;
+        display: flex;
+        gap: 0.5rem;
+      }
+
+      .pagination button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        font-size: 2rem;
+        padding: 4px 6px;
+        border-radius: 5px;
+        color: black;
+        transition: background-color 0.2s ease, color 0.2s ease;
+      }
+
+      .pagination button.disabled{
+        cursor: default;
+      }
+
+      .pagination button:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+        color: #000000cc;
+      }
+
+      .pagination button.disabled{
+        background-color: transparent;
+        color:hsl(0, 0.00%, 26.70%);
+      }
+
+      .page-info {
+        font-weight: bold;
+        font-size: 0.7rem;
+      }
+    </style>
+
+    <section class="table">
+      <div class="table__header">
+        <div class="table__header-box">
+          <button class="filter-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>filter-check</title>
+              <path d="M12 12V19.88C12.04 20.18 11.94 20.5 11.71 20.71C11.32 21.1 10.69 21.1 10.3 20.71L8.29 18.7C8.06 18.47 7.96 18.16 8 17.87V12H7.97L2.21 4.62C1.87 4.19 1.95 3.56 2.38 3.22C2.57 3.08 2.78 3 3 3H17C17.22 3 17.43 3.08 17.62 3.22C18.05 3.56 18.13 4.19 17.79 4.62L12.03 12H12M17.75 21L15 18L16.16 16.84L17.75 18.43L21.34 14.84L22.5 16.25L17.75 21" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="table__body"></div>
+      <div class="table__footer">
+        <div class="table__footer-left">
+          
+          <span>${this.data.meta.total} registros en total, mostrando ${this.data.meta.size} por página</span>
+        </div>
+        <div class="table__footer-right">
+          <div class="pagination">
+            <button class="pagination-button ${this.data.meta.currentPage === 1 ? 'disabled' : ''}" data-page="1">&laquo;</button>
+            <button class="pagination-button ${this.data.meta.currentPage === 1 ? 'disabled' : ''}" data-page="${this.data.meta.currentPage > 1 ? this.data.meta.currentPage - 1 : 1}">&lsaquo;</button>
+            <span class="page-info">${this.data.meta.currentPage} de ${this.data.meta.pages}</span>
+            <button class="pagination-button ${this.data.meta.currentPage === this.data.meta.pages ? 'disabled' : ''}"  data-page="${this.data.meta.currentPage < this.data.meta.pages ? this.data.meta.currentPage + 1 : this.data.meta.currentPage}">&rsaquo;</button>
+            <button class="pagination-button ${this.data.meta.currentPage === this.data.meta.pages ? 'disabled' : ''}" data-page="${this.data.meta.pages}">&raquo;</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    `
+
+    const tableBody = this.shadow.querySelector('.table__body')
+    tableBody.innerHTML = ''
+
+    this.data.rows.forEach(element => {
+      const userBox = document.createElement('div')
+      const upperRow = document.createElement('div')
+      const editIcon = document.createElement('button')
+      const deleteIcon = document.createElement('button')
+      const data = document.createElement('div')
+      const ul = document.createElement('ul')
+
+      userBox.classList.add('table__body__user-box')
+      upperRow.classList.add('user-box__upper-row')
+      editIcon.classList.add('edit-icon')
+      deleteIcon.classList.add('delete-icon')
+      data.classList.add('user-box__data')
+
+      editIcon.dataset.id = element.id
+      deleteIcon.dataset.id = element.id
+
+      editIcon.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>pencil</title>
+          <path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
+        </svg>`
+      deleteIcon.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>delete</title>
+          <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+        </svg>`
+
+      ul.innerHTML = `
+        <li>Nombre: ${element.name}</li>
+        <li>Email: ${element.email}</li>
+        <li>Fecha de creación: ${element.createdAt}</li>
+        <li>Fecha de actualización: ${element.updatedAt}</li>
+      `
+
+      upperRow.append(editIcon, deleteIcon)
+      data.appendChild(ul)
+      userBox.append(upperRow, data)
+      tableBody.appendChild(userBox)
+    })
+
+    this.renderButtons()
+  }
+
+  renderButtons () {
+    this.shadow.querySelector('.table').addEventListener('click', async event => {
+      const editBtn = event.target.closest('.edit-icon')
+      const deleteBtn = event.target.closest('.delete-icon')
+      const filterBtn = event.target.closest('.filter-icon')
+
+      if (editBtn) {
+        const id = editBtn.dataset.id
+        try {
+          const response = await fetch(`${this.endpoint}/${id}`)
+          if (!response.ok) throw response
+          const data = await response.json()
+          store.dispatch(showFormElement({ endPoint: this.endpoint, data }))
+        } catch {
+          document.dispatchEvent(new CustomEvent('notice', {
+            detail: { message: 'No se ha podido recuperar el dato', type: 'error' }
+          }))
+        }
+      }
+
+      if (deleteBtn) {
+        const id = deleteBtn.dataset.id
+        document.dispatchEvent(new CustomEvent('showDeleteModal', {
+          detail: { endpoint: this.endpoint, elementId: id }
+        }))
+      }
+
+      if (filterBtn) {
+        document.dispatchEvent(new CustomEvent('showFilterModal', {
+          detail: {
+            endpoint: this.endpoint
+          }
+        }))
+      }
+
+      if (event.target.closest('.pagination-button') && !event.target.closest('.pagination-button').classList.contains('.disabled')) {
+        const page = event.target.closest('.pagination-button').dataset.page
+        let endpoint = `${this.endpoint}?page=${page}`
+        this.loadData(endpoint).then(() => this.render())
+      }
+      if (this.filterQuery) {
+        endpoint = `${endpoint}&${this.filterQuery}`
+      }
+    })
+  }
+}
+
+customElements.define('promoters-table-component', PromoterTable)
